6c3be180b9c6f6c83c3dbba324ce90b74f3f3b7d
piandas <santiagobustosp@gmail.com>
2023-08-07 17:19:21 -0500

    directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { ChangeVowelsPipe } from './pipes/change-vowels.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ChangeVowelsPipe } from './pipes/change-vowels.pipe';
     NavComponent,
     ReversePipe,
     TimeAgoPipe,
-    ChangeVowelsPipe
+    ChangeVowelsPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
